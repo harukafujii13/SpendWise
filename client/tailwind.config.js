4923f57268396d6fe01de4afab39174737705582
Haruka <hfujii1013@gmail.com>
2023-08-26 13:28:07 -0700

    chore: add extend

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -6,7 +6,29 @@ export default {
     './node_modules/tailwind-datepicker-react/dist/**/*.js',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primay-lavender': '#E1E5F2',
+        'primary-columbiaBlue': '#BFDBF7',
+        'primary-teal': '#1F7A8C',
+        'primary-gunmetal': '#022B3A',
+      },
+      fontFamily: {
+        nunito: ['Nunito', 'sans-serif'],
+      },
+      animation: {
+        shake: 'shake 0.5s ease-in-out',
+      },
+      keyframes: {
+        shake: {
+          '0%': { transform: 'translateX(0)' },
+          '25%': { transform: 'translateX(10px)' },
+          '50%': { transform: 'translateX(-10px)' },
+          '75%': { transform: 'translateX(10px)' },
+          '100%': { transform: 'translateX(0)' },
+        },
+      },
+    },
   },
   plugins: [],
 };
