3e99e472ed0a291a6fbd8b0cc9477f4fc08b1ed0
Haruka <hfujii1013@gmail.com>
2023-08-28 12:20:01 -0700

    update: Home, Login, Signup page

---
+++ client/src/page/SignupPage/SignupPage.jsx
@@ -0,0 +1,91 @@
+import React from 'react';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+
+const SignupPage = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .post('http://localhost:8000/register', { name, email, password })
+      .then((result) => console.log(result));
+    navigate('/login').catch((err) => console.log(err));
+  };
+
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <div className="p-6 bg-white rounded-lg shadow-lg w-96">
+        <div className="text-2xl font-bold mb-4">Register</div>
+        <form onSubmit={handleSubmit}>
+          <div className="mb-4">
+            <label
+              htmlFor="name"
+              className="block text-sm font-medium text-gray-600">
+              Name
+            </label>
+            <input
+              type="text"
+              placeholder="Enter your name"
+              autoComplete="off"
+              name="name"
+              className="mt-1 p-2 w-full border rounded-md"
+              onChange={(e) => setName(e.target.value)}
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              htmlFor="email"
+              className="block text-sm font-medium text-gray-600">
+              Email
+            </label>
+            <input
+              type="email"
+              placeholder="Enter email"
+              autoComplete="off"
+              name="email"
+              className="mt-1 p-2 w-full border rounded-md"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              htmlFor="password"
+              className="block text-sm font-medium text-gray-600">
+              Password
+            </label>
+            <input
+              type="password"
+              placeholder="Enter password"
+              name="password"
+              className="mt-1 p-2 w-full border rounded-md"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <button
+            type="submit"
+            className="w-full p-2 bg-blue-500 text-white rounded-md hover:bg-blue-600">
+            Register
+          </button>
+        </form>
+        <div className="text-center mt-4">
+          <p className="text-sm">Already have an account?</p>
+          <Link
+            to="/login"
+            className="text-blue-500 hover:underline">
+            Login
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SignupPage;
+
+//rfce
